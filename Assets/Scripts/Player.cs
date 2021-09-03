661d521f42bf8aedac808a38af9d93e0d45e8d7b
sarahunf <reis.sarahtbr@gmail.com>
2021-09-02 22:37:44 -0300

    begin rules

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using CardControllers;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Player:  CardsInTurn
 {
+    //private int pid;
+    public int score;
+    
     //player ID used x card on this turn
     //use drag and drop events maybe
     //remember the player knows which cards he gets, not the other way around
-    
-    //set lists for every type of card here and score accordingly
-    //use rules to set rules and scoring for player
+
+
 }
